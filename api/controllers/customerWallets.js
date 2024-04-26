9544fe96c90f816573eeb22dee8616963bf75cee
Claudio Marcio <claudiomarcio89@hotmail.com>
2024-04-26 12:53:46 -0300

    projeto Node ApiRest com express

---
+++ api/controllers/customerWallets.js
@@ -0,0 +1,30 @@
+const uuidv4 = require('uuidv4');
+
+module.exports = app => {
+    const customerWalletsDB = app.data.customerWallets;
+    const controller = {};
+
+    const {
+        customerWallets: customerWalletsMock,
+    } = customerWalletsDB;
+
+    controller.listCustomerWallets = (req, res) => res.status(200).json(customerWalletsDB);
+  
+    controller.saveCustomerWallets = (req, res) => {
+        customerWalletsMock.data.push({
+          id: uuidv4.uuid(),
+          parentId: uuidv4.uuid(),
+          name: req.body.name,
+          birthDate: req.body.birthDate,
+          cellphone: req.body.cellphone,
+          phone: req.body.phone,
+          email: req.body.email,
+          occupation: req.body.occupation,
+          state: req.body.state,
+        });
+    
+        res.status(201).json(customerWalletsMock);
+      };
+
+    return controller;
+}
